7dbc7aa9a143d9d1d195d5546d8475d670490b55
Leonard <lbielka@gmail.com>
2024-12-19 14:18:43 +0100

    Added README, logout, updated docstrings

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# todo remove unsafe code
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '123'
